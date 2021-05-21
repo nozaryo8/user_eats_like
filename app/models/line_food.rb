58f1daf6cf688f0af85fe301f82ca47485804ae9
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-05-21 16:47:45 +0900

    ルーティング作成

--- app/models/line_food.rb
+++ app/models/line_food.rb
@@ -1,12 +1,13 @@
 class LineFood < ApplicationRecord
   belongs_to :food
   belongs_to :restaurant
-  belongs_to :order, optional: true
+
+  belongs_to :order, optional: true #教材上は不要
 
   validates :count, numericality: { greater_than: 0 }
 
   #scopeでは全てのLineFoodからwhereでactive: trueなもの一覧を返す
-  scope :active, -> { where(active: true) }
+  scope :active, -> { where(active: true) } #LineFood.active　←この形で使用する
 
   #scopeに引数を与え、restaurant_idが特定の店舗IDではないもの一覧を返してくれます。
   scope :other_restaurant, -> (picked_restaurant_id) { where.not(restaurant_id: picked_restaurant_id) }
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #今回はAPIモードでrails newしたのでViewが存在しないよって、画面上で呼ばれるルーティングは不要。必要なのは、APIのURLのみ
+  #v1を付ける理由は開発中にバージョン変更する時にスイッチングしやすくなるから
+  #namespace: hogeで名前空間を付与する。これはコントローラーをグルーピングする。作成するファイルはapp/controllers/api/v1/というディレクトリに作成する
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants do
+        resources :foods, only: [:index]
+      end
+      resources :line_foods, only: [:index ,:create]
+      put 'line_foods/replace', to: 'line_foods#replace' #getリクエストでも良い
+      resources :orders, only: [:create]
+    end
+  end
 end
