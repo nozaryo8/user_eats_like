008209ed91ed788c1e9771df73863405dc0526d4
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-05-23 22:54:28 +0900

    レストラン一覧ページのヘッダーを作成

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,34 @@
 import React, { Fragment, useEffect } from 'react';
+// styled-componentsを使えるようにする
+import styled from 'styled-components';
+
 // apis
 import { fetchRestaurants } from '../apis/restaurants';
+
+// images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+//ページの一番上部にあるヘッダー全体を定義
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+//ヘッダーのロゴ
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+//トップページ真ん中のメイン画像
+const MainCover = styled.img`
+  height: 600px;
+`;
 
 export const Restaurants = () => {
   useEffect(() => {
@@ -12,7 +40,12 @@ export const Restaurants = () => {
 
   return (
     <Fragment>
-      レストラン一覧
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
     </Fragment>
   )
 }
